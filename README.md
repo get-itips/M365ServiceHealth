d3d00cafa8f619265b616a6b5be40a0417c5170a
Andres Mariano Gorzelany <36666927+get-itips@users.noreply.github.com>
2021-10-26 00:49:20 +0000

    improving readme and colors

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # M365ServiceHealth
+
+## Introduction
+
+Started to build this module for educational purposes and also because I am a Microsoft 365 Multi-Tenant administrator and wanted to have the service status being refreshed in a PowerShell window (or better, in a Windows Terminal pane) while working.
+
+## Cmdlets
+The module is pretty simple, so far you can find:
+
+- Get-M365ServiceHealth
+- Get-M365ServiceHealthToken
+
+## Future
+I plan in adding more cmdlets based on the [service communications API of Microsoft Graph](https://docs.microsoft.com/graph/api/resources/service-communications-api-overview).
+
+Contributions are very well welcomed!
--- src/public/func_Get-M365ServiceHealth.ps1
+++ src/public/func_Get-M365ServiceHealth.ps1
@@ -2,20 +2,42 @@ Function Get-M365ServiceHealth {
 		[CmdletBinding()]
 		Param( 
                 [Parameter(Position = 0, Mandatory = $true)][String]$Access_Token
-
+				[Parameter(Position = 0, Mandatory = $false)][String]$Refresh
 		) 
 		Process { 
             $apiUrl = 'https://graph.microsoft.com/v1.0/admin/serviceAnnouncement/healthOverviews'
-            $Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $access_token"} -Uri $apiUrl -Method Get
-			$services = @()
-			foreach ($entry in $data.value){
-				$obj = New-Object -TypeName PSObject
-				$obj | Add-Member -MemberType NoteProperty -Name Service -value $entry.service
-				$obj | Add-Member -MemberType NoteProperty -Name Status -value $entry.Status
-				
-				$services+=$obj
-				
-			}
-			return $services
+            while($true){
+				$Data = Invoke-RestMethod -Headers @{Authorization = "Bearer $access_token"} -Uri $apiUrl -Method Get
+				$services = @()
+				foreach ($entry in $data.value){
+					$obj = New-Object -TypeName PSObject
+					$obj | Add-Member -MemberType NoteProperty -Name Service -value $entry.service
+					$obj | Add-Member -MemberType NoteProperty -Name Status -value $entry.Status
+					
+					$services+=$obj
+					
+				}
+				$services | Format-Table Service, @{
+
+				Label = "Status"
+				Expression = 
+					{
+
+						switch($_.status)
+						{
+							'restoringService' { $color="96";break}
+							'serviceOperational' {$color="92"; break}
+							default { $color = "0" }
+						}
+						$e = [char]27
+						"$e[${color}m$($_.status)${e}[0m"
+					}
+				}
+				$now=Get-Date
+				Write-Host "Last updated $now"
+				Start-Sleep -Seconds $Refresh
+				Clear-Host
+			}	
+
 		} 
 	}
